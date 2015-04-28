d95e263a0f3efe5ade0a3685d1cb05de3cd8fd41
kuramal <linxxnil@126.com>
2015-04-28 22:41:13 +0800

    start my encrypt project hahaha

--- main.c
+++ main.c
@@ -1,13 +1,58 @@
-#include<stdio.h>
+/*
+* ---------------------------------------------------------------------
+* #############################################
+* Copyright (c) 2015-2035 kuramal All rights reserved.
+* #############################################
+*
+* Name:  main.c
+* Date:  2015-04-28 21:47
+* Author:   kuramal
+* Desc:  
+*
+* ---------------------------------------------------------------------
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-int main(){
+int file_porter(char *src_dir, char *dst_dir)
+{
+	int ret;
+	DIR *cur_dir;
+	struct dirent *dir_ptr = NULL;
+	
+	if (access(dst_dir, F_OK) != 0){
 
-	int i = 0;
-
-	for (;i < 5; i++ll){
-
-		printf("hello world\n");
-		printf("github test\n");
+		ret = mkdir(dst_dir, 0);
+		if (ret){
+			printf("mkdir %s ERR\n", dst_dir);
+			return ret;
+		}
 	}
+
+	cur_dir = opendir(src_dir);
+
+	while( (dir_ptr = (readdir(cur_dir))) != NULL){
+		printf("%s \n", dir_ptr->d_name);
+	}
+
+	closedir(cur_dir);
+	
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	
+	char *src_dir = "./";
+	char *encrypt_dir = "./wakeup";
+
+	file_porter(src_dir, encrypt_dir);	
+
+        return 0;
 }
 
---
+++ secret.c
@@ -0,0 +1,24 @@
+/*
+* ---------------------------------------------------------------------
+* #############################################
+* Copyright (c) 2015-2035 kuramal All rights reserved.
+* #############################################
+*
+* Name:  secret.c
+* Date:  2015-04-28 21:50
+* Author:  kuramal
+* Desc:  
+*
+*
+*
+* ---------------------------------------------------------------------
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+
